8ff6f42e4bd6c63191d077d70ddd7ff6ac6ab569
Sam Selikoff <sam.selikoff@gmail.com>
2023-04-10 18:53:55 -0400

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,24 @@
+import { ReactNode } from "react";
 import { Switch } from "react-aria-components";
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center h-full">
-      <Switch>
-        <div className="indicator" />
-        Low power mode
-      </Switch>
+    <div className="flex flex-col justify-center items-center h-full">
+      <div className="flex flex-col space-y-3">
+        <Toggle>Airplane Mode</Toggle>
+        <Toggle>Wi-Fi</Toggle>
+      </div>
     </div>
+  );
+}
+
+function Toggle({ children }: { children: ReactNode }) {
+  return (
+    <Switch className="inline-flex items-center text-white space-x-4 group">
+      <div className="flex w-11 h-6 rounded-full items-center border-2 border-transparent bg-zinc-600 group-data-[selected]:bg-green-500 transition duration-200 ease-in-out group-data-[focus-visible]:ring-2 ring-offset-2 ring-offset-zinc-900">
+        <div className="bg-white shadow w-5 h-5 rounded-full origin-right transition-all duration-200 ease-in-out group-data-[pressed]:w-7 group-data-[selected]:translate-x-5 group-data-[selected]:group-data-[pressed]:translate-x-3" />
+      </div>
+      <span>{children}</span>
+    </Switch>
   );
 }
