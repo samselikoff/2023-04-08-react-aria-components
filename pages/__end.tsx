397756b54a118aecb2a6ea38dd97ff5fdf3818d8
Sam Selikoff <sam.selikoff@gmail.com>
2023-04-12 19:55:42 -0400

    wip

--- pages/__end.tsx
+++ pages/__end.tsx
@@ -1,25 +1,29 @@
-import { ReactNode } from "react";
+import { ComponentProps, ReactNode, useState } from "react";
 import { Switch } from "react-aria-components";
 
 export default function Page() {
+  let [airplaneMode, setAirplaneMode] = useState(true);
+
   return (
-    <div className="flex flex-col justify-center items-center h-full scale-[3]">
-      <div className="flex flex-col space-y-3">
-        <Toggle>Airplane Mode</Toggle>
+    <div className="flex flex-col items-center px-6 justify-center min-h-full">
+      <div className="flex flex-col space-y-3 w-72">
+        <Toggle isSelected={airplaneMode} onChange={setAirplaneMode}>
+          Airplane Mode: {airplaneMode ? "On" : "Off"}
+        </Toggle>
         <Toggle>Wi-Fi</Toggle>
       </div>
     </div>
   );
 }
 
-function Toggle({ children }: { children: ReactNode }) {
+function Toggle({
+  children,
+  ...props
+}: { children: ReactNode } & ComponentProps<typeof Switch>) {
   return (
-    <Switch
-      className="inline-flex items-center text-white space-x-4 group tap"
-      style={{ WebkitTapHighlightColor: "transparent" }}
-    >
-      <div className="flex w-10 h-6 rounded-full items-center border-2 border-transparent bg-zinc-600 group-data-[selected]:bg-green-500 transition group-data-[focus-visible]:ring-2 ring-offset-2 ring-offset-zinc-900">
-        <div className="bg-white shadow w-5 h-5 rounded-full origin-right transition-[width] group-data-[pressed]:w-77 delay-1000 group-data-[selected]:ml-4 group-data-[selected]:group-data-[pressed]:ml-2" />
+    <Switch {...props} className="inline-flex items-center space-x-4 group">
+      <div className="flex w-10 h-6 rounded-full items-center border-2 border-transparent bg-zinc-600 group-data-[selected]:bg-green-500 transition group-data-[focus-visible]:ring-2 ring-offset-2 ring-offset-zinc-900 duration-200">
+        <div className="bg-white shadow w-5 h-5 rounded-full origin-right transition-all group-data-[pressed]:w-7 group-data-[selected]:ml-4 group-data-[selected]:group-data-[pressed]:ml-2 duration-300" />
       </div>
       <span>{children}</span>
     </Switch>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,29 @@
-import { ReactNode } from "react";
+import { ComponentProps, ReactNode, useState } from "react";
 import { Switch } from "react-aria-components";
 
 export default function Page() {
+  let [airplaneMode, setAirplaneMode] = useState(true);
+
   return (
-    <div className="flex flex-col items-center pt-20 px-6">
-      <div className="flex flex-col space-y-3">
-        <Toggle>Airplane Mode</Toggle>
+    <div className="flex flex-col items-center px-6 justify-center min-h-full">
+      <div className="flex flex-col space-y-3 w-72">
+        <Toggle isSelected={airplaneMode} onChange={setAirplaneMode}>
+          Airplane Mode: {airplaneMode ? "On" : "Off"}
+        </Toggle>
         <Toggle>Wi-Fi</Toggle>
-        {/* {[...Array(100).keys()].map((i) => (
-          <p key={i}>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Repellendus aspernatur voluptatibus doloremque quas expedita
-            consequuntur modi iste et veniam totam, optio eum dolorum quidem,
-            temporibus, labore eos praesentium ab! Repellendus.
-          </p>
-        ))} */}
       </div>
     </div>
   );
 }
 
-function Toggle({ children }: { children: ReactNode }) {
+function Toggle({
+  children,
+  ...props
+}: { children: ReactNode } & ComponentProps<typeof Switch>) {
   return (
-    <Switch className="inline-flex items-center space-x-4 group">
+    <Switch {...props} className="inline-flex items-center space-x-4 group">
       <div className="flex w-10 h-6 rounded-full items-center border-2 border-transparent bg-zinc-600 group-data-[selected]:bg-green-500 transition group-data-[focus-visible]:ring-2 ring-offset-2 ring-offset-zinc-900 duration-200">
-        <div className="bg-white shadow w-5 h-5 rounded-full origin-right transition-all group-data-[pressed]:w-7 group-data-[pressed]:delay-75 group-data-[selected]:ml-4 group-data-[selected]:group-data-[pressed]:ml-2 duration-300" />
+        <div className="bg-white shadow w-5 h-5 rounded-full origin-right transition-all group-data-[pressed]:w-7 group-data-[selected]:ml-4 group-data-[selected]:group-data-[pressed]:ml-2 duration-300" />
       </div>
       <span>{children}</span>
     </Switch>
