c5fc442741c5cfc7b6a28e69bfcbfd4fb9e7c3fd
Sam Selikoff <sam.selikoff@gmail.com>
2023-04-12 21:25:35 -0400

    wip

--- pages/__begin.tsx
+++ pages/__begin.tsx
@@ -1,9 +1,15 @@
+import { ReactNode } from "react";
+
 export default function Page() {
   return (
     <div className="flex flex-col items-center px-6 justify-center min-h-full text-center">
-      <div className="flex flex-col space-y-3 w-72">
-        Let&apos;s build a toggle
+      <div className="flex flex-col w-72">
+        <Toggle>Airplane Mode</Toggle>
       </div>
     </div>
   );
+}
+
+function Toggle({ children }: { children: ReactNode }) {
+  return <>{children}</>;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export default function Page() {
   return (
     <div className="flex flex-col items-center px-6 justify-center min-h-full text-center">
-      <div className="flex flex-col space-y-3 w-72">
+      <div className="flex flex-col w-72">
         <Toggle>Airplane Mode</Toggle>
       </div>
     </div>
