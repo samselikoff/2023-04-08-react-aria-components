9df25e08532b055ed0b7e99a06ee050dec46bfd8
Sam Selikoff <sam.selikoff@gmail.com>
2023-04-12 20:22:37 -0400

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 export default function Page() {
   return (
     <div className="flex flex-col items-center px-6 justify-center min-h-full text-center">
-      <div className="flex flex-col space-y-3 w-72">
-        Let&apos;s build a toggle
-      </div>
+      <div className="flex flex-col space-y-3 w-72">Let's build a toggle</div>
     </div>
   );
 }
